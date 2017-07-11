74162e34dd41154799cf02c2028ce79ad3fe4f75
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-07-10 21:37:49 -0700

    Working little server that on every connection will return Hello

--- Server.java
+++ Server.java
@@ -1,12 +1,22 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.OutputStream;
 
 public class Server {
   public static void main(String[] args) throws java.io.IOException {
-    
+ 		// Instantiate the server:
     ServerSocket server = new ServerSocket(80);
     System.out.println("Server has started on localhost:80.\r\nWaiting for a connection...");
-    Socket client = server.accept();
-    System.out.println("A client connected.");
+    // Make the server listen while the program is running:
+    while (true) {
+	    Socket client = server.accept();
+	    System.out.println("A client connected: " + client.getInetAddress());
+	    // Return something to the client:
+	    byte[] b = {'H', 'e', 'l', 'l', 'o'};
+	    OutputStream out = client.getOutputStream();
+	    out.write(b);
+	    // Close the socket:
+	    client.close();
+    }
   }
 }
