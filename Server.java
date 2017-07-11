337ba4ec2f5e73d6bdcff7d5586bb7e583069027
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-07-10 20:32:12 -0700

    Created little repository and added a super simple socket server.

---
+++ Server.java
@@ -0,0 +1,12 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+  public static void main(String[] args) throws java.io.IOException {
+    
+    ServerSocket server = new ServerSocket(80);
+    System.out.println("Server has started on 127.0.0.1:80.\r\nWaiting for a connection...");
+    Socket client = server.accept();
+    System.out.println("A client connected.");
+  }
+}
