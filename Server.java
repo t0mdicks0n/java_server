783185cc8d844dd0ae5745dec895d2d9bddf1a00
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-07-10 20:33:51 -0700

    Changed console log.

--- Server.java
+++ Server.java
@@ -5,7 +5,7 @@ public class Server {
   public static void main(String[] args) throws java.io.IOException {
     
     ServerSocket server = new ServerSocket(80);
-    System.out.println("Server has started on 127.0.0.1:80.\r\nWaiting for a connection...");
+    System.out.println("Server has started on localhost:80.\r\nWaiting for a connection...");
     Socket client = server.accept();
     System.out.println("A client connected.");
   }
